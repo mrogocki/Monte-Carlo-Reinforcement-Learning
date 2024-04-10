e75af51b383029b113a16c0e941da4ea9802a708
M. Rogocki <56718881+mrogocki@users.noreply.github.com>
2024-04-10 23:11:24 +0200

    add episode generation stochastic and mc prediction

--- Monte_Carlo.py
+++ Monte_Carlo.py
@@ -38,3 +38,52 @@ for episodes in range(3):
             print(f"Complete episode is: {episode}")
             break
 
+####### Generate episode with specific stochastic episode
+
+def generate_stochastic_episode(env):
+    state = env.reset()
+    episode = []
+    
+    while True:
+        # If card more than 17, than say STICK with 70%, otherwise 30%
+        prob_pos = 0.7
+        probs = [prob_pos, 1 - prob_pos] if state[0] > 17 else [1 - prob_pos, prob_pos]
+        
+        # Select action based on probability
+        action = np.random.choice(np.arange(2), p=probs)
+        next_state, rewards, done, info, _ = env.step(action)
+        episode.append([next_state, action, rewards])
+
+        # Move to next state
+        state = next_state
+        
+        # Terminate if done
+        if done:
+            break
+
+    return episode
+
+
+####### Generate a Monte Carlo Prediction
+# Implementation of a first visit policy prediction
+
+def mc_prediction(env, number_episodes, generate_episode, gamma=1):
+    # Initialize visit count
+    N = defaultdict(lambda: np.zeros(env.action_space.n))
+    # Initialize return sums for all state action pairs
+    return_sums = defaultdict(lambda: np.zeros(env.action_space.n))
+    # Initialize Q table
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+
+    for index_episode in range(number_episodes+1):
+        episode = generate_episode(env)
+        # Unpack list
+        states, actions, rewards = zip(*episode)
+        # Discount the reward with gamma, default = 1
+        discount = [gamma ** i for i in range(len(rewards)+1)]
+        for index, state in enumerate(states):
+            # Update the Q table if first visit
+            return_sums[state][actions[index]] += sum(rewards[index:]*discount[:-(1+index)])
+            N[state][actions[index]] += 1.0
+            Q[state][actions[index]] = return_sums[state][actions[index]]
+
